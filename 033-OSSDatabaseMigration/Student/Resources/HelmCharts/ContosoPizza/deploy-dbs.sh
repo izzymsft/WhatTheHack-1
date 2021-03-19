476ba0c4fb50cdaf257adf4d9c4f7da748346eb3
Pete Rodriguez <perktime@users.noreply.github.com>
2021-03-19 16:12:36 -0500

    Fixed some bugs in the deploy-dbs.sh and corresponding yaml

--- 033-OSSDatabaseMigration/Student/Resources/HelmCharts/ContosoPizza/deploy-dbs.sh
+++ 033-OSSDatabaseMigration/Student/Resources/HelmCharts/ContosoPizza/deploy-dbs.sh
@@ -1,8 +1,18 @@
+status="Running"
 # Install the Kubernetes Resources
 helm upgrade --install wth-postgresql ../PostgreSQL116 --set infrastructure.password=OCPHack8
 
+for ((i = 0 ; i < 30 ; i++)); do
+    pgStatus=$(kubectl -n postgresql get pods --no-headers -o custom-columns=":status.phase")
+
+
+    if [ "$pgStatus" != "$status" ]; then
+        sleep 10
+    fi
+done
+
 # Get the postgres pod name
-$pgPodName=$(kubectl -n postgresql get pods --no-headers -o custom-columns=":metadata.name")
+pgPodName=$(kubectl -n postgresql get pods --no-headers -o custom-columns=":metadata.name")
 
 #Copy pg.sql to the postgresql pod
 kubectl -n postgresql cp ./pg.sql $pgPodName:/tmp/pg.sql
@@ -13,25 +23,33 @@ kubectl -n postgresql exec deploy/postgres -it -- /usr/bin/psql -U postgres -f /
 # Install the Kubernetes Resources
 helm upgrade --install wth-mysql ../MySQL57 --set infrastructure.password=OCPHack8
 
-# Get the MySQL pod name
-$mysqlPodName=$(kubectl -n mysql get pods --no-headers -o custom-columns=":metadata.name")
+for ((i = 0 ; i < 30 ; i++)); do
+    mysqlStatus=$(kubectl -n mysql get pods --no-headers -o custom-columns=":status.phase")   
 
-#Copy mysql.sql to the postgresql pod
-kubectl -n mysql cp ./mysql.sql $mysqlPodName:/tmp/mysql.sql
+    if [ "$mysqlStatus" != "$status" ]; then
+        sleep 10
+    fi
+done
+
+# Get the MySQL pod name
+mysqlPodName=$(kubectl -n mysql get pods --no-headers -o custom-columns=":metadata.name")
 
 # Use this to connect to the database server
 
-kubectl -n mysql exec deploy/mysql -it -- /usr/bin/mysql -u root -pOCPHack8 < mysql.sql 
+kubectl -n mysql exec deploy/mysql -it -- /usr/bin/mysql -u root -pOCPHack8 <./mysql.sql
 
-cd ../ContosoPizza
+postgresClusterIP=$(kubectl -n postgresql get svc -o json |jq .items[0].spec.clusterIP |tr -d '"')
+mysqlClusterIP=$(kubectl -n mysql get svc -o json |jq .items[0].spec.clusterIP |tr -d '"')
 
-$postgresClusterIP=$(kubectl -n postgresql get svc -o json -o json |jq .items[0].spec.clusterIP |tr -d '"')
-$mysqlClusterIP=$(kubectl -n mysql get svc -o json -o json |jq .items[0].spec.clusterIP |tr -d '"')
+sed "s/XXX.XXX.XXX.XXX/$clusterip/" ./values-postgresql.yaml >temp_postgresql.yaml && mv temp_postgresql.yaml ./values-postgresql.yaml
+sed "s/XXX.XXX.XXX.XXX/$clusterip/" ./values-mysql.yaml >temp_mysql.yaml && mv temp_mysql.yaml ./values-mysql.yaml
 
-sed "s/XXX.XXX.XXX.XXX/$clusterip/" values-postgres.yaml >temp_mysql.yaml && mv temp_postgres.yaml values-postgres.yaml
-sed "s/XXX.XXX.XXX.XXX/$clusterip/" values-mysql.yaml >temp_mysql.yaml && mv temp_mysql.yaml values-mysql.yaml
+helm upgrade --install mysql-contosopizza . -f ./values.yaml -f ./values-mysql.yaml
 
-helm upgrade --install mysql-contosopizza ./ContosoPizza -f ./ContosoPizza/values.yaml -f ./ContosoPizza/values-mysql.yaml
+helm upgrade --install postgres-contosopizza . -f ./values.yaml -f ./values-postgresql.yaml
 
-helm upgrade --install postgres-contosopizza ./ContosoPizza -f ./ContosoPizza/values.yaml -f ./ContosoPizza/values-postgresql.yaml
+postgresAppIP=$(kubectl -n contosoapppostgres get svc -o json  |jq .items[0].status.loadBalancer.ingress[0].ip|tr -d '"')
+mysqlAppIP=$(kubectl -n contosoappmysql get svc -o json  |jq .items[0].status.loadBalancer.ingress[0].ip |tr -d '"')
 
+echo "Pizzeria app on MySQL is ready at http://$mysqlAppIP:8081/pizzeria"
+echo "Pizzeria app on PostgreSQL is ready at http://$postgresAppIP:8082/pizzeria"
