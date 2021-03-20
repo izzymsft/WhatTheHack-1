800df842187bfb26315e88ae0808c067b06cefa1
Pete Rodriguez <perktime@users.noreply.github.com>
2021-03-20 08:18:38 -0500

    Fixed some bugs due to some values not being ready due to container creation

--- 033-OSSDatabaseMigration/Student/Resources/HelmCharts/ContosoPizza/deploy-dbs.sh
+++ 033-OSSDatabaseMigration/Student/Resources/HelmCharts/ContosoPizza/deploy-dbs.sh
@@ -1,4 +1,8 @@
 status="Running"
+
+# Install the Kubernetes Resources
+helm upgrade --install wth-mysql ../MySQL57 --set infrastructure.password=OCPHack8
+
 # Install the Kubernetes Resources
 helm upgrade --install wth-postgresql ../PostgreSQL116 --set infrastructure.password=OCPHack8
 
@@ -20,19 +24,14 @@ kubectl -n postgresql cp ./pg.sql $pgPodName:/tmp/pg.sql
 # Use this to connect to the database server
 kubectl -n postgresql exec deploy/postgres -it -- /usr/bin/psql -U postgres -f /tmp/pg.sql
 
-# Install the Kubernetes Resources
-helm upgrade --install wth-mysql ../MySQL57 --set infrastructure.password=OCPHack8
 
 for ((i = 0 ; i < 30 ; i++)); do
     mysqlStatus=$(kubectl -n mysql get pods --no-headers -o custom-columns=":status.phase")   
 
     if [ "$mysqlStatus" != "$status" ]; then
-        sleep 10
+        sleep 30
     fi
 done
-
-# Get the MySQL pod name
-mysqlPodName=$(kubectl -n mysql get pods --no-headers -o custom-columns=":metadata.name")
 
 # Use this to connect to the database server
 
@@ -41,12 +40,28 @@ kubectl -n mysql exec deploy/mysql -it -- /usr/bin/mysql -u root -pOCPHack8 <./m
 postgresClusterIP=$(kubectl -n postgresql get svc -o json |jq .items[0].spec.clusterIP |tr -d '"')
 mysqlClusterIP=$(kubectl -n mysql get svc -o json |jq .items[0].spec.clusterIP |tr -d '"')
 
-sed "s/XXX.XXX.XXX.XXX/$clusterip/" ./values-postgresql.yaml >temp_postgresql.yaml && mv temp_postgresql.yaml ./values-postgresql.yaml
-sed "s/XXX.XXX.XXX.XXX/$clusterip/" ./values-mysql.yaml >temp_mysql.yaml && mv temp_mysql.yaml ./values-mysql.yaml
+sed "s/XXX.XXX.XXX.XXX/$postgresClusterIP/" ./values-postgresql-orig.yaml >temp_postgresql.yaml && mv temp_postgresql.yaml ./values-postgresql.yaml
+sed "s/XXX.XXX.XXX.XXX/$mysqlClusterIP/" ./values-mysql-orig.yaml >temp_mysql.yaml && mv temp_mysql.yaml ./values-mysql.yaml
 
 helm upgrade --install mysql-contosopizza . -f ./values.yaml -f ./values-mysql.yaml
 
 helm upgrade --install postgres-contosopizza . -f ./values.yaml -f ./values-postgresql.yaml
+
+for ((i = 0 ; i < 30 ; i++)); do
+    appStatus=$(kubectl -n contosoappmysql get svc -o json  |jq .items[0].status.loadBalancer.ingress[0].ip |tr -d '"')   
+
+    if [ "$appStatus" == "null" ]; then
+        sleep 30
+    fi
+done
+
+for ((i = 0 ; i < 30 ; i++)); do
+    appStatus=$(kubectl -n contosoappmysql get svc -o json  |jq .items[0].status.loadBalancer.ingress[0].ip |tr -d '"')   
+
+    if [ "$appStatus" == "null" ]; then
+        sleep 30
+    fi
+done
 
 postgresAppIP=$(kubectl -n contosoapppostgres get svc -o json  |jq .items[0].status.loadBalancer.ingress[0].ip|tr -d '"')
 mysqlAppIP=$(kubectl -n contosoappmysql get svc -o json  |jq .items[0].status.loadBalancer.ingress[0].ip |tr -d '"')
